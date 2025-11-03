104b095c0f2151842b8046078b28fe223c80b066
NguyenDThai <thainguyen4646@gmail.com>
2025-11-03 15:44:39 +0700

    fix add promise for api put order

--- src/app/api/admin/order/[id]/route.ts
+++ src/app/api/admin/order/[id]/route.ts
@@ -5,7 +5,7 @@ import { connectDB } from "@/lib/db";
 
 export async function PUT(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { status } = await req.json();
